2796d4193b692e1341bc4b2a827bf60bf6235374
NikitaEvs <nikitaevsykov2@gmail.com>
2021-11-07 18:43:27 +0300

    Adapt example.cpp for STM32F411

--- Src/example.cpp
+++ Src/example.cpp
@@ -2,7 +2,9 @@
  * @brief Example of the library usage with led blinking
  * while data is processed in the background
  */
-#include "Specific/STM32F103C8/master.hpp"
+// Choose the using driver (the corresponding DEVICE variable
+// should be set as the CMake argument)
+#include "Specific/STM32F411CE6/master.hpp"
 
 #include "Utils/buffer.hpp"
 #include "Utils/led.hpp"
@@ -10,7 +12,6 @@
 
 #include "Hardware/adc.hpp"
 #include "Hardware/hardware_layout.hpp"
-
 
 int main() {
   // Get main supervisor instance and enable default clocking
@@ -24,24 +25,27 @@ int main() {
   SD::Hardware::AsyncUART uart(master, SD::Hardware::UART::UART1);
 
   // Create non-blocking ADC class, add one channel and configure it
-  SD::Hardware::ADC</*Blocking=*/false> adc(master,
+  SD::Hardware::ADCUnit</*Blocking=*/false> adc(master,
                                             SD::Hardware::ADCDevice::ADC_1);
-  adc.AddChannel({SD::Hardware::GPIO::A, SD::Hardware::Pin::Pin1});
+  adc.AddChannel({SD::Hardware::GPIO::A, SD::Hardware::Pin::Pin0});
   adc.Init();
 
   // Create storage with the asynchronous support
-  SD::Utils::AsyncBuffer<uint16_t, /*size=*/16> buffer(master);
+  SD::Utils::AsyncBuffer<uint16_t, /*size=*/64 + 1> buffer(master,
+                      /* delimiter=*/ std::numeric_limits<uint16_t>::max());
 
   // Connect UART1 as a destination point for the buffer
   buffer.ConnectDestination(SD::Hardware::UART::UART1,
-                            {SD::Hardware::DMADevice::DMA_1,
-                             SD::Hardware::DMAChannel::Channel4},
+                            {.device = SD::Hardware::DMADevice::DMA_2,
+                             .channel = SD::Hardware::DMAChannel::Channel4,
+                             .stream = SD::Hardware::DMAStream::Stream7},
                             uart.GetDMAInSettings());
 
   // Connect ADC1 as a source point for the buffer
   buffer.ConnectSource(SD::Hardware::ADCDevice::ADC_1,
-                       {SD::Hardware::DMADevice::DMA_1,
-                        SD::Hardware::DMAChannel::Channel1},
+                       {.device = SD::Hardware::DMADevice::DMA_2,
+                        .channel = SD::Hardware::DMAChannel::Channel0,
+                        .stream = SD::Hardware::DMAStream::Stream4},
                        adc.GetDMAOutSettings());
 
   // Set input and output callbacks
@@ -51,8 +55,9 @@ int main() {
     buffer.EnableOutput();
   });
 
-  buffer.SetOutputCallback([&buffer]{
+  buffer.SetOutputCallback([&buffer, &adc]{
     buffer.DisableOutput();
+    adc.Read();
     buffer.EnableInput();
   });
 
