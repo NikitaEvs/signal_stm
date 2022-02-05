070eed0a391b55e8e87cdabd930018c1b8558627
NikitaEvs <nikitaevsykov2@gmail.com>
2022-02-05 22:22:57 +0300

    Add missing example

--- Examples/STM32F411CE6/adc.cpp
+++ Examples/STM32F411CE6/adc.cpp
@@ -1,4 +1,79 @@
-//
-// Created by hutu on 05.02.2022.
-//
+/**
+ * @brief Example of the library usage with led blinking
+ * while data is processed in the background
+ */
 
+// Choose the using driver (the corresponding DEVICE variable
+// should be set as the CMake argument)
+#include "Specific/STM32F411CE6/master.hpp"
+
+#include "Utils/buffer.hpp"
+#include "Utils/led.hpp"
+#include "Utils/logger.hpp"
+
+#include "Hardware/adc.hpp"
+#include "Hardware/hardware_layout.hpp"
+
+int main() {
+  // Configure peripherals usage
+  const auto kUsingUART = SD::Hardware::UART::UART1;
+  const auto kUsingADC = SD::Hardware::ADCDevice::ADC_1;
+
+  // Get main supervisor instance and enable default clocking
+  auto& master = SD::Specific::GetMasterInstance();
+  master.EnableClocking();
+
+  // Create a system LED instance
+  SD::Utils::LED led(master);
+
+  // Create non-blocking UART class
+  SD::Hardware::AsyncUART uart(master, kUsingUART);
+
+  // Create non-blocking ADC class, add one channel and configure it
+  SD::Hardware::ADCUnit</*Blocking=*/false> adc(master,
+                                                kUsingADC);
+  adc.AddChannel({SD::Hardware::GPIO::A, SD::Hardware::Pin::Pin0});
+  adc.Init();
+
+  // Create storage with the asynchronous support
+  SD::Utils::AsyncBuffer<uint16_t, /*size=*/64 + 1> buffer(master,
+      /* delimiter=*/ std::numeric_limits<uint16_t>::max());
+
+  // Connect UART1 as a destination point for the buffer
+  buffer.ConnectDestination(SD::Hardware::UART::UART1,
+                            master.GetDMAMapping(kUsingUART, /*isTX=*/true),
+                            uart.GetDMAInSettings());
+
+  // Connect ADC1 as a source point for the buffer
+  buffer.ConnectSource(SD::Hardware::ADCDevice::ADC_1,
+                       master.GetDMAMapping(kUsingADC),
+                       adc.GetDMAOutSettings());
+
+  // Set input and output callbacks
+  // which will be executed in interruption handlers
+  buffer.SetInputCallback([&buffer]{
+    buffer.DisableInput();
+    buffer.EnableOutput();
+  });
+
+  buffer.SetOutputCallback([&buffer, &adc]{
+    buffer.DisableOutput();
+    adc.Read();
+    buffer.EnableInput();
+  });
+
+  // Finalize ADC initialization
+  adc.Calibrate();
+  adc.Read();
+
+  // Enable DMA for the ADC
+  buffer.EnableInput();
+
+  // Do something in the main loop while data is processed in the background
+  while (true) {
+    led.On();
+    master.Delay(1000);
+    led.Off();
+    master.Delay(1000);
+  }
+}
