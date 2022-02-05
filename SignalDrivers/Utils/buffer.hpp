0cd1de8fcbd21093ed84c854aa9fc40e36bec003
NikitaEvs <nikitaevsykov2@gmail.com>
2022-02-05 21:29:15 +0300

    Add DataProcessor system for creating data transformer's pipelines

--- SignalDrivers/Utils/buffer.hpp
+++ SignalDrivers/Utils/buffer.hpp
@@ -4,6 +4,8 @@
 
 #include "Hardware/dma.hpp"
 
+#include "Utils/data.hpp"
+
 namespace SD {
 
 namespace Utils {
@@ -25,8 +27,12 @@ class AsyncBuffer : public Hardware::DMAPipe {
                      Hardware::DMAPort dma_port,
                      const Hardware::DMASettings& settings) {
     SetInput(dma_port);
-    GetInput()->Init(device, reinterpret_cast<uint32_t>(buffer_.data()),
-                     CalculateSize(settings.dest_data_size), settings);
+    GetInput()->Init(device,
+                     reinterpret_cast<uint32_t>(buffer_.data()),
+                     CalculateSize(settings.dest_data_size,
+                                   size,
+                                   has_delimiter_),
+                     settings);
   }
 
   template<typename DeviceT>
@@ -34,24 +40,67 @@ class AsyncBuffer : public Hardware::DMAPipe {
                           Hardware::DMAPort dma_port,
                           const Hardware::DMASettings& settings) {
     SetOutput(dma_port);
-    GetOutput()->Init(device, reinterpret_cast<uint32_t>(buffer_.data()),
+
+    uint32_t data_ptr = 0;
+    std::size_t data_size = 0;
+    std::size_t variable_size = 0;
+    bool data_has_delimiter = false;
+
+    if (processor_view_.IsValid()) {
+      data_ptr = processor_view_.GetDataPtr();
+      data_size = processor_view_.Size();
+      variable_size = processor_view_.VariableSize();
+      data_has_delimiter = processor_view_.HasDelimiter();
+    } else {
+      data_ptr = reinterpret_cast<uint32_t>(GetDataPointer());
+      data_size = Size();
+      variable_size = sizeof(T);
+      data_has_delimiter = has_delimiter_;
+    }
+
+    GetOutput()->Init(device,
+                      data_ptr,
                       CalculateSize(settings.dest_data_size,
-                                    /* with_delimiter = */true),
+                                    data_size,
+                                    variable_size,
+                                    data_has_delimiter,
+                                    /*with_delimiter=*/true),
                       settings);
   }
 
   void SetInputCallback(std::function<void()>&& callback) {
     auto& input = GetInput();
     if (input) {
-      input->SetCallback(std::move(callback));
+      if (processor_view_.IsValid()) {
+        input->SetCallback([this, callback = std::move(callback)](){
+          this->DisableInput();
+
+          this->processor_view_.Process(GetDataPointer(), DataSize());
+          callback();
+        });
+      } else {
+        input->SetCallback([this, callback = std::move(callback)](){
+          this->DisableInput();
+
+          callback();
+        });
+      }
     }
   }
 
   void SetOutputCallback(std::function<void()>&& callback) {
     auto& output = GetOutput();
     if (output) {
-      output->SetCallback(std::move(callback));
+      output->SetCallback([this, callback = std::move(callback)](){
+        this->DisableOutput();
+
+        callback();
+      });
     }
+  }
+
+  void SetDataProcessor(DataProcessorView<T>&& view) {
+    processor_view_ = std::move(view);
   }
 
   void EnableInput() {
@@ -112,6 +161,10 @@ class AsyncBuffer : public Hardware::DMAPipe {
     return size;
   }
 
+  [[nodiscard]] std::size_t DataSize() const {
+    return has_delimiter_? size - 1 : size;
+  }
+
   auto begin() {
     return buffer_.begin();
   }
@@ -124,8 +177,13 @@ class AsyncBuffer : public Hardware::DMAPipe {
   std::array<T, size> buffer_;
   bool has_delimiter_;
 
-  [[nodiscard]] uint32_t CalculateSize(Hardware::DMASize dma_size,
-                                       bool with_delimiter = false) const {
+  DataProcessorView<T> processor_view_ = DataProcessorView<T>::Invalid();
+
+  [[nodiscard]] static uint32_t CalculateSize(Hardware::DMASize dma_size,
+                                              std::size_t data_size,
+                                              std::size_t variable_size,
+                                              bool has_delimiter,
+                                              bool with_delimiter = false) {
     // TODO: Check that it isn't implementation defined
     uint32_t chunk_size = 0;
 
@@ -145,14 +203,22 @@ class AsyncBuffer : public Hardware::DMAPipe {
     }
 
     std::size_t buffer_bytes_size;
-    if (!with_delimiter && has_delimiter_) {
-      buffer_bytes_size = sizeof(T) * (size - 1);
+    if (!with_delimiter && has_delimiter) {
+      buffer_bytes_size = variable_size * (data_size - 1);
     } else {
-      buffer_bytes_size = sizeof(T) * size;
+      buffer_bytes_size = variable_size * data_size;
     }
 
     auto chunk_number = buffer_bytes_size / chunk_size;
     return chunk_number > 0 ? chunk_number : 1;
+  }
+
+  [[nodiscard]] static uint32_t CalculateSize(Hardware::DMASize dma_size,
+                                       std::size_t data_size,
+                                       bool has_delimiter,
+                                       bool with_delimiter = false) {
+    return CalculateSize(dma_size, data_size, sizeof(T),
+                         has_delimiter, with_delimiter);
   }
 };
 
